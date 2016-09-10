a4bd3d8ff412d0351091369329d2d5584f30bc8d
Niranjani <niranjani123@gmail.com>
2016-09-10 11:11:43 +0530

    Delete CheckForColumn.m

--- CheckForColumn.m
+++
@@ -1,6 +0,0 @@
-function [cnt] = CheckForColumn(col_no,check_cols)
-%col_no=7; check_cols=sig;
-col=size(check_cols,2);
-for i=1:col
-    if(check_cols(i)==col_no), cnt(1)=1; cnt(2)=i; break; else cnt(1)=0; cnt(2)=0; end
-end
